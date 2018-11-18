3adf409da3c0bc480a59de7729f6704b56fa9c33
shailahirji <shaila.hirji@bellevuecollege.edu>
2018-11-18 10:32:29 -0800

    current location name pop-up

--- TripAdvisor/my-googlemap/src/App.js
+++ TripAdvisor/my-googlemap/src/App.js
@@ -32,18 +32,29 @@ export class MapContainer extends Component {
         this.setState({
           showingInfoWindow:false,
           activeMarker: null,
+          selectedPlace:null
         });
       }
     };
 
   render() {
     //console.log(this.props.state);
-  console.log('showing window '+this.state.showingInfoWindow);
-  console.log('on marker click '+ this.state.onMarkerClick);
-  console.log("visible "+this.state.visible);
-  if(this.state.selectedPlace.mapCenter!==undefined){
-  console.log('selected place '+this.state.selectedPlace.mapCenter.name_loc);
+  // console.log('showing window '+this.state.showingInfoWindow);
+  // console.log('on marker click '+ this.state.onMarkerClick);
+  // console.log("visible "+this.state.visible);
+  // if(this.state.selectedPlace.mapCenter!==undefined){
+  // console.log('selected place '+this.state.selectedPlace.mapCenter.name_loc);
+  let show_name;
+  if(this.state.showingInfoWindow){
+    show_name=(<div>
+      <h4>{this.state.selectedPlace.mapCenter.name_loc}</h4>
+    </div>)
+  }else{
+    show_name=(<div>
+      <h4>{""}</h4>
+    </div>)
   }
+  console.log(show_name);
 return(
   <CurrentLocation
   centerAroundCurrentLocation
@@ -51,7 +62,7 @@ return(
   >
   <Marker
   onClick={this.onMarkerClick}  
-  name={this.state.selectedPlace.mapCenter!==undefined ? this.state.selectedPlace.mapCenter.name_loc:undefined}
+  //name={this.state.selectedPlace.mapCenter!==undefined ? this.state.selectedPlace.mapCenter.name_loc:undefined}
   /> 
   <InfoWindow
     marker={this.state.activeMarker}
@@ -59,7 +70,7 @@ return(
     onClose={this.onClose}
     >
     <div>
-      <h4>{this.state.selectedPlace.name}</h4>
+      {show_name}
     </div>
 
     </InfoWindow>
@@ -68,7 +79,7 @@ return(
 
 );
 }
-}
+  }
 
 export default GoogleApiWrapper(
 (props)=>({
