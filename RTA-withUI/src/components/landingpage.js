6dcf20bbd4fae5bccf976e0113e736bdabf5b9eb
Frederick Wirtz <frederickwirtz@Fredericks-MBP.domain>
2018-12-02 18:18:47 -0800

    updated image location

--- RTA-withUI/src/components/landingpage.js
+++ RTA-withUI/src/components/landingpage.js
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
-import pic from '/Users/shaila/Downloads/RoadTripAdvisor-develop/RTA-withUI/src/components/roadtrip.jpg';
+import pic from "./roadtrip.jpg";
 
 class Landing extends Component {
   render() {
     return (
       <div style={{ width: "100%", margin: "auto" }}>
-       
-            <img
-              src={pic}
-              alt="avatar"
-              className="avatar-img"  
-            />
-      
-       
-  
+        <img src={pic} alt="avatar" className="avatar-img" />
       </div>
     );
   }
