f7ad0ff88eba2235e9317a885a50961900326e85
Frederick Wirtz <frederickwirtz@Fredericks-MacBook-Pro.local>
2019-02-19 10:37:21 -0800

    Clear Project

--- README.txt
+++
@@ -1,5 +0,0 @@
-README file
-
-For RTA-with UI
--> change the path of the image in landingpage.js relative to your directory
--> change googlemaps API key 
--- TripAdvisor/Trial/sygic_geocode.js
+++
@@ -1,2 +0,0 @@
-$.getJSON("https://geocoding.api.sygic.com/v0/api/geocode?country=USA&city=Seattle&key=88uIu8H6xVy3DEbk8bKLxp2pm", function(data){
-     console.log(data)});
