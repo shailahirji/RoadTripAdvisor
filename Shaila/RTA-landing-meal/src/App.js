a634af1727e4dffc545ca22fc8019bfe1554df81
shailahirji <shaila.hirji@bellevuecollege.edu>
2019-02-25 18:35:11 -0800

    combined meal preferences to landing page

---
+++ Shaila/RTA-landing-meal/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component, } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+import { Provider } from 'react-redux';
+
+import Home from './components/Home.js';
+import Header from './components/Header.js';
+import Profile from './components/Profile.js';
+import Card from './components/Card.js';
+import MapContainer from './components/MapContainer';
+import MyMapComponent from './components/MyMapComponent'
+import MealPref from './components/MealPreferences'
+
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+const store = require('./reducers').init();
+
+class App extends Component {
+  render() {
+
+    return <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div className="bg-main">
+
+            <Switch>
+
+              <Route exact path="/" component={Home} />
+              <Route exact path="/Profile" component={Card} />
+              <Route exact path="/Map" component={MapContainer} />
+              <Route exact path="/mealpref" component={MealPref} />
+
+
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </Provider>;
+
+
+
+  }
+}
+
+export default App;
